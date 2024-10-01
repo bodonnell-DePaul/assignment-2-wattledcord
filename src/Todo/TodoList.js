cb56c7ad21ae991aa64ff3b7b18682a53348d35d
B Madhava Reddy <bmadhavreddy2015@gmail.com>
2024-10-01 00:12:28 -0500

    Added dynamic nature to list group and tab.

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,8 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './TodoList.css';
-import { Button, Container, Form, ListGroupItem, ListGroup,Row,Col, Tab, Nav } from "react-bootstrap";
+import todos from "../todoItems";
+import {Button, Container, Form, ListGroup, Row, Col, Tab} from "react-bootstrap";
 
 export default function TodoList() {
+    const [todolist, setTodoList] = useState([]);
+
+    useEffect(() => {
+        const todoList = todos.map((todo) => {
+            let todoVar = {
+                title: todo.title,
+                description: todo.description,
+                dueDate: todo.dueDate,
+
+            };
+            let date = new Date(todo.dueDate);
+            let currentDate = new Date();
+// Convert dates to UTC timestamps
+            let utc1 =
+                Date.UTC(date.getFullYear(), date.getMonth(), date.getDate());
+            let utc2 =
+                Date.UTC(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+
+// Calculate the time difference in milliseconds
+            let timeDiff = Math.abs(utc1 - utc2);
+
+// Convert milliseconds to days
+            let daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+            if (daysDiff > 7) {
+                todoVar['variant'] = 'primary';
+            } else if (daysDiff > 4 && daysDiff <= 7) {
+                todoVar['variant'] = 'success';
+            } else if (daysDiff > 2 && daysDiff <= 4) {
+                todoVar['variant'] = 'warning';
+            } else {
+                todoVar['variant'] = 'danger';
+            }
+            return todoVar;
+        });
+        setTodoList(todoList);
+    }, []);
 
     return (
         <Container>
@@ -19,27 +56,41 @@ export default function TodoList() {
                         <Form.Label>Date</Form.Label>
                         <Form.Control type="date" placeholder="Enter Date"></Form.Control>
                     </Form.Group>
-                    <Button variant="primary" type="submit">Add</Button>
+                    <div className="d-grid">
+                        <Button variant="primary" type="submit">Add</Button>
+                    </div>
+
                 </Form>
-                <Tab.Container defaultActiveKey="#first">
-                <Row>
-                    <Col>
-                        <ListGroup>
-                           <ListGroup.Item action href="#first">First</ListGroup.Item>
-                           <ListGroup.Item action href="#second">second</ListGroup.Item>
-                           <ListGroup.Item action href="#third">third</ListGroup.Item>
-                           <ListGroup.Item action href="#fourth">fourth</ListGroup.Item>
-                        </ListGroup>
+                <Tab.Container defaultActiveKey="#1">
+                    <Row>
+                        <Col>
+                            <ListGroup>
+                                {
+                                    todolist.map((todo, index) => {
+                                        return (
+                                            <ListGroup.Item action href={`#${index}`} key={index} variant={todo.variant}>
+                                                {todo.title}
+                                            </ListGroup.Item>
+                                        )
+                                    })
+                                }
+                            </ListGroup>
                         </Col>
                         <Col>
                             <Tab.Content>
-                                <Tab.Pane eventKey="#first">First Item</Tab.Pane>
-                                <Tab.Pane eventKey="#second">Second Item</Tab.Pane>
-                                <Tab.Pane eventKey="#third">Third Item</Tab.Pane>
-                                <Tab.Pane eventKey="#fourth">Fourth Item</Tab.Pane>
+                                {
+                                    todolist.map((todo, index) => {
+                                        return (
+                                            <Tab.Pane eventKey={`#${index}`} key={index}>
+                                                <p>{todo.description}</p>
+                                                <p>{todo.dueDate}</p>
+                                            </Tab.Pane>
+                                        )
+                                    })
+                                }
                             </Tab.Content>
                         </Col>
-                </Row>
+                    </Row>
                 </Tab.Container>
             </Container>
         </Container>
--- src/todoItems.js
+++ src/todoItems.js
@@ -5,7 +5,7 @@
     aliquip ex ea commodo consequat. Duis aute irure dolor in
     reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
     pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-    culpa qui officia deserunt mollit anim id est laborum.`, dueDate: '2024-04-03' },
+    culpa qui officia deserunt mollit anim id est laborum.`, dueDate: '2024-10-03' },
 
     { title: 'Todo 2', description: `Sed auctor nunc nec nisi ultrices, in molestie nibh mattis. 
     Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
@@ -15,10 +15,12 @@
     { title: 'Todo 3', description: `Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
     Suspendisse potenti. Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. Vivamus nec nisi nec nunc mattis molestie. 
     Sed auctor nunc nec nisi ultrices, in molestie nibh mattis. Pellentesque habitant morbi tristique 
-    senectus et netus et malesuada fames ac turpis egestas.`, dueDate: '2024-04-09' },
+    senectus et netus et malesuada fames ac turpis egestas.`, dueDate: '2024-10-09' },
 
     { title: 'Todo 4', description: `Suspendisse potenti. Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. 
     Vivamus nec nisi nec nunc mattis molestie. Sed auctor nunc nec nisi ultrices, in molestie nibh mattis. 
     Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse potenti. 
-    Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. `, dueDate: '2024-04-11' },
+    Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. `, dueDate: '2024-10-2' },
   ];
+
+export default todos;
